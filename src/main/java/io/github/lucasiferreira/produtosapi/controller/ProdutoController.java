60ca2a9bdd58a697d1ef338b348fad15dadf132a
Lucas <lucas.iferreir@gmail.com>
2025-05-08 12:37:34 -0300

    Criando endpoint para retornar um objeto pelo ID

--- src/main/java/io/github/lucasiferreira/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/lucasiferreira/produtosapi/controller/ProdutoController.java
@@ -4,11 +4,9 @@ package io.github.lucasiferreira.produtosapi.controller;
 import io.github.lucasiferreira.produtosapi.model.Produto;
 import io.github.lucasiferreira.produtosapi.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -19,12 +17,16 @@ public class ProdutoController {
     private ProdutoRepository repository;
 
     @PostMapping
-    public Produto salvar(@RequestBody  Produto produto){
-        System.out.println("Produto recebido: "+ produto);
+    public Produto salvar(@RequestBody Produto produto) {
+        System.out.println("Produto recebido: " + produto);
         var id = UUID.randomUUID().toString();
         produto.setId(id);
-
         repository.save(produto);
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable String id) {
+        return repository.findById(id).orElse(null);
     }
 }
